c5207a50944f37a68fdb9c9c071f6359fafca2ef
Ikmal Karim <ikmalkarim97@gmail.com>
2019-02-25 18:13:38 +0800

    first commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Mohammad Ikmal Bin Muhamad Abdul Karim 1622067
+        </Text>
+        
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
